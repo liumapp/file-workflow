d1c1142d9ba3f9c821c042cd644aa0e109518919
liumapp <liumapp.com@gmail.com>
2018-06-05 15:28:46 +0800

    add iview in main js

--- flow-ui/src/main.js
+++ flow-ui/src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
-
+Vue.use(iView)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
