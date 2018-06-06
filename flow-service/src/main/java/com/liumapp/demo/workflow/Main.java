51d81d81e4644f6ab9c40aa1c37230b69db2c837
liumapp <liumapp.com@gmail.com>
2018-06-06 11:21:38 +0800

    update flow-service

--- flow-service/src/main/java/com/liumapp/demo/workflow/Main.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/Main.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by liumapp on 9/28/17.
- * E-mail:liumapp.com@gmail.com
- * home-page:http://www.liumapp.com
+ * @author liumapp
+ * @file Main.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/6/18
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
--- flow-service/src/main/java/com/liumapp/demo/workflow/controller/IndexController.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/controller/IndexController.java
@@ -1,30 +1,19 @@
 package com.liumapp.demo.workflow.controller;
 
-import com.liumapp.demo.workflow.entity.Guest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by liumapp on 9/28/17.
- * E-mail:liumapp.com@gmail.com
- * home-page:http://www.liumapp.com
+ * @author liumapp
+ * @file IndexController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/6/18
  */
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping("")
 public class IndexController {
 
-    @Autowired
-    private Guest guest;
 
-    @GetMapping("/")
-    public String index (ModelMap model) {
-
-        model.addAttribute("name" , guest.getName());
-        model.addAttribute("sex" , guest.getSex());
-        return "index";
-    }
 
 }
