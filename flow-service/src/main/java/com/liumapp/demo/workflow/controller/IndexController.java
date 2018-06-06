da5f3e78ceeb3e14420c65ebc5b63ac1d52982b3
liumapp <liumapp.com@gmail.com>
2018-06-06 11:31:20 +0800

    handle form

--- flow-service/src/main/java/com/liumapp/demo/workflow/controller/IndexController.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.liumapp.demo.workflow.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.liumapp.demo.workflow.entity.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class IndexController {
 
-
+    @RequestMapping("/info")
+    public String handleForm (@RequestBody User user) {
+        return JSON.toJSONString();
+    }
 
 }
