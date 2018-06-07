23317f11812b17823768fc96381bd89f146aa5d6
liumapp <liumapp.com@gmail.com>
2018-06-07 09:05:44 +0800

    add util package

---
+++ flow-service/src/main/java/com/liumapp/demo/workflow/util/FileManager.java
@@ -0,0 +1,17 @@
+package com.liumapp.demo.workflow.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liumapp
+ * @file FileManager.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/7/18
+ */
+@Component
+public class FileManager {
+
+
+
+}
