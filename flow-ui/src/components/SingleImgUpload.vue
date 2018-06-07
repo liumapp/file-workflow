db7c9942266d13ec93edce5d1d9703a337b69a6e
liumapp <liumapp.com@gmail.com>
2018-06-07 14:22:41 +0800

    ready to add single img upload component

---
+++ flow-ui/src/components/SingleImgUpload.vue
@@ -0,0 +1,14 @@
+/**
+ * @file SingleImgUpload.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/7/18
+ */
+<template>
+</template>
+<script>
+export default {
+  name: 'single-img-upload'
+}
+</script>
