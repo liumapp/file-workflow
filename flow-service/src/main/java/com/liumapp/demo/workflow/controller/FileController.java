30eb7ecd7c55b556bbecc2185778b0c14bd1acb2
liumapp <liumapp.com@gmail.com>
2018-06-07 14:11:57 +0800

    get img pic done

--- flow-service/src/main/java/com/liumapp/demo/workflow/controller/FileController.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/controller/FileController.java
@@ -3,8 +3,12 @@ package com.liumapp.demo.workflow.controller;
 import com.alibaba.fastjson.JSON;
 import com.liumapp.demo.workflow.util.FileManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,10 +28,19 @@ public class FileController {
     @Autowired
     private FileManager fileManager;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @RequestMapping("/")
     public String upload (@RequestParam("file") MultipartFile file) throws IOException {
         fileManager.save(file);
         return JSON.toJSONString("success");
+    }
+
+    @RequestMapping(value = "/get", produces="image/*")
+    @ResponseBody
+    public ResponseEntity<?> getImg (@RequestParam String filename) {
+        return ResponseEntity.ok(resourceLoader.getResource("file:" + fileManager.getSavepath() + filename));
     }
 
 }
--- flow-service/src/main/java/com/liumapp/demo/workflow/util/FileManager.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/util/FileManager.java
@@ -5,7 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author liumapp
