e1ba2f16c1a5b5a4fbe6e6ed93c8900f1e5b7082
liumapp <liumapp.com@gmail.com>
2018-06-07 09:02:53 +0800

    add file controller

--- flow-service/src/main/java/com/liumapp/demo/workflow/controller/FileController.java
+++ flow-service/src/main/java/com/liumapp/demo/workflow/controller/FileController.java
@@ -1,27 +1,26 @@
 package com.liumapp.demo.workflow.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.liumapp.demo.workflow.entity.User;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author liumapp
- * @file IndexController.java
+ * @file FileController.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
- * @date 6/6/18
+ * @date 6/7/18
  */
 @RestController
-@RequestMapping("")
-public class IndexController {
+@RequestMapping("file")
+public class FileController {
 
-    @RequestMapping("info")
-    public String handleForm (@RequestBody User user) {
+    @RequestMapping("/")
+    public String upload (@RequestParam("file") MultipartFile file) {
+
         return JSON.toJSONString("success");
     }
-
-
 
 }
