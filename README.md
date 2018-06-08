7090ba02a6532740b38ca99d0445e09b7ee43a26
liumapp <liumapp.com@gmail.com>
2018-06-08 10:16:27 +0800

    add rm-image shell

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # file-workflow
 Display the file upload workflow with iView
+
+## how to use
+
+* run ./build-image.sh
+
+* run docker.compose up -d
+
+* wait a second , and open your browser visit http://localhost
+
+* you can find the result here : 
+
+
+
+## attention
+
+* if you want to rebuilt flow-ui , plz run :
+
+		cd flow-ui
+
+		cp -r ./dist/* ../nginx/www/flowui	
+
+	to make nginx in docker get the source . 
+
+* if you want to rebuilt flow-service , plz run :
+	
+		./rm-image.sh
+
+	than 	
+
+		./build-image.sh			
+
+		
---
+++ rm-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker rmi liumapp/flow-service:v1.0.0
+
+
+
